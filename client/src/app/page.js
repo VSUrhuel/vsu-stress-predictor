248e6242e1b7ae42a5f1bf0e70d22eb02c3b8a22
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-11-20 10:15:48 +0800

    Updated HTML

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,15 +1,11 @@
 "use client";
 import Form from "@/components/Form";
 
-import {NextUIProvider} from "@nextui-org/system";
 
 export default function Home() {
   return (
-    <NextUIProvider>
     <div>
-      
       <Form></Form>
     </div>
-    </NextUIProvider>
   );
 }
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -59,89 +59,65 @@ export default function Form() {
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-center mt-6">Stress Predictor</h1>
         <p className="mb-6 text-lg font-normal text-center text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">Test your stress level by inputting the neccesary data in the web!</p>
         <form class="max-w-md mx-auto">
-            <div class="relative z-0 w-full mb-5 group">
-                <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-            </div>
-            <div class="relative z-0 w-full mb-5 group">
-                <input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-            </div>
-            <div class="relative z-0 w-full mb-5 group">
-                <input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-            </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
-                <div class="relative z-0 w-full mb-5 group">
-                    <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
-                </div>
-            </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
-                <div class="relative z-0 w-full mb-5 group">
-                    <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
-                </div>
-                <div class="relative z-0 w-full mb-5 group">
-                    <input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                    <label for="floating_company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
-                </div>
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-            </form>
-        <form onSubmit={handleSubmit}>
             <InputField
-            label="Study hours per day:"
-            type="number"
-            name="studyHours"
-            value={formData.studyHours}
-            onChange={handleChange}
-            placeholder="Enter study hours"
-            required
+                label="Study hours per day:"
+                type="number"
+                name="studyHours"
+                value={formData.studyHours}
+                onChange={handleChange}
+                placeholder="Enter study hours"
+                required
             />
-                
-            <label>Extracurricular hours per day:</label>
-            <input type="number"
-                    name="extracurricularHours"
-                    value={formData.extracurricularHours}
-                    onChange={handleChange} 
-                    required
+            <InputField
+                label="Extracurricular hours per day:"
+                type="number"
+                name="extracurricularHours"
+                value={formData.extracurricularHours}
+                onChange={handleChange}
+                placeholder="Enter extracurricular hours"
+                required={true}
             />
-            <label>Sleep hours per day:</label>
-            <input type="number"
-                    name="sleepHours"
-                    value={formData.sleepHours}
-                    onChange={handleChange} 
-                    required
+            <InputField
+                label="Sleep hours per day:"
+                type="number"
+                name="sleepHours"
+                value={formData.sleepHours}
+                onChange={handleChange}
+                placeholder="Enter sleep hours"
+                required={true}
             />
-            <label>Social hours per day:</label>
-            <input type="number"
-                    name="socialHours"
-                    value={formData.socialHours}
-                    onChange={handleChange} 
-                    required
+            <InputField
+                label="Social hours per day:"
+                type="number"
+                name="socialHours"
+                value={formData.socialHours}
+                onChange={handleChange}
+                placeholder="Enter social hours"
+                required={true}
             />
-            <label>Physical activity hours per day:</label>
-            <input type="number"
-                    name="physicalHours"
-                    value={formData.physicalHours}
-                    onChange={handleChange} 
-                    required
-             />
-             <label>GPA:</label>
-            <input type="number"
-                    name="gpa"
-                    value={formData.gpa}
-                    onChange={handleChange} 
-                    required
-             />
-            <button type="submit">Submit</button>
+            <InputField
+                label="Physical activity hours per day:"
+                type="number"
+                name="physicalHours"
+                value={formData.physicalHours}
+                onChange={handleChange}
+                placeholder="Enter physical activity hours"
+                required={true}
+            />
+            <InputField
+                label="GPA:"
+                type="number"
+                name="gpa"
+                value={formData.gpa}
+                onChange={handleChange}
+                placeholder="Enter GPA"
+                required={true}
+            />
 
+            
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
+        
         {prediction && (
             <div>
                 <h2>Prediction: {prediction}</h2>
