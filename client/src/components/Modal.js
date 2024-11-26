bd3e94b7755f3f325ed76461c0834ce2ec1a6871
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-11-27 01:41:26 +0800

    Updated

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,7 +3,6 @@ import { X, Download } from 'lucide-react'
 import { useRef } from 'react'
 import { textarea } from '@material-tailwind/react'
 import { m } from 'framer-motion'
-
 function Modal({ onClose, content }) {
     const modalRef = useRef()
     let colorClass = ''
@@ -63,20 +62,50 @@ function Modal({ onClose, content }) {
             onClick={closeModal}
             className="fixed inset-0 bg-black bg-opacity-80 backdrop-blur-sm flex justify-center items-center"
         >
-            <div className="mt-10 flex flex-col gap-5 text-white">
-                <button onClick={onClose} className="place-self-end">
-                    <X size="30" />
-                </button>
-                <div
-                    className={`bg-green-600 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4 ${colorClass}`}
-                >
-                    <h1 className="text-3xl font-extrabold">Stress Level</h1>
+            <div className="bg-white mt-10 flex flex-col w-1.3 text-white rounded-xl overflow-hidden w-1/3">
+                <div className="flex flex-row-reverse mr-2 mt-2">
+                    <button
+                        onClick={onClose}
+                        className="place-self-end text-black"
+                    >
+                        <X size="30" />
+                    </button>
+                </div>
+                <div className="justify-center align-middle content-center flex">
                     <img
+                        className="pt-4"
                         src={imageSrc}
-                        alt="Stress Level"
-                        className="w-20 h-20"
-                    />
-                    {message}
+                        width={200}
+                        height={200}
+                    ></img>
+                </div>
+                <div className=" px-20 py-10 flex flex-col gap-5 items-center mx-4">
+                    <h2 className={`text-3xl font-extrabold ${colorClass}`}>
+                        {content !== undefined && content !== null
+                            ? content.toUpperCase()
+                            : 'No result available'}
+                    </h2>
+                    <p className="text-center w-full text-gray-600 text-sm ">
+                        {message}
+                    </p>
+                </div>
+                <div className="bg-[#146939] px-4 py-3 text-center flex flex-col gap-1">
+                    <div className="flex justify-center gap-2">
+                        <img src="/info.png" width={30} height={15}></img>
+                        <h3 className="text-base font-semibold">Learn More</h3>
+                    </div>
+
+                    <p className="text-sm">
+                        Visit the{' '}
+                        <a
+                            href="https://www.who.int/news-room/questions-and-answers/item/stress"
+                            className="text-blue-400 underline font-semibold"
+                        >
+                            {' '}
+                            World Health Organization
+                        </a>{' '}
+                        website for more information on stress management.
+                    </p>
                 </div>
             </div>
         </div>
